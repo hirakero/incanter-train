61677daf5906731c4fd7c594a0a081b20e49bd35
kero_hirake <k16.h.white@gmail.com>
2022-02-24 09:48:45 +0900

    ヒストグラムをファイルに保存、まで

--- src/incanter_tutorial/core.clj
+++ src/incanter_tutorial/core.clj
@@ -1,6 +1,28 @@
-(ns incanter-tutorial.core)
+(ns incanter-tutorial.core
+  (:require [incanter.core :as incanter]
+            [incanter.stats :as status]
+            [incanter.charts :as charts]
+            [incanter.io :as io]
+            [incanter.pdf]
+            [incanter.svg]
+            [clojure.core.matrix :as matrix]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;https://github.com/incanter/incanter
+
+(-> (status/sample-normal 1000) ;標準正規分布から1,000個の値をサンプリング
+    charts/histogram ;ヒストグラムを
+    incanter/view) ;表示
+
+(def my-plot (charts/function-plot incanter/sin -10 10 )) ;-10から10の範囲にわたる正弦関数のプロット
+(incanter/view my-plot) ;表示
+(incanter/save my-plot "plot.png") ;pngで保存
+(incanter.pdf/save-pdf my-plot "plot.pdf")  
+(incanter.svg/save-svg my-plot "plot.svg")  
+
+
+(comment
+  (charts/conj)
+  (incanter/conj-rows)
+  (status/sin 100)
+
+  )
