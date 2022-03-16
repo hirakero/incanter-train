382bb5d6cfa1765c9660df966a9e993c04a2dd85
kero_hirake <k16.h.white@gmail.com>
2022-03-16 16:45:44 +0900

    グラフの練習

--- src/incanter_tutorial/core.clj
+++ src/incanter_tutorial/core.clj
@@ -2,6 +2,7 @@
   (:require [incanter.core :as incanter]
             [incanter.stats :as status]
             [incanter.charts :as charts]
+            [incanter.datasets :as datasets]
             [incanter.io :as io]
             [incanter.pdf]
             [incanter.svg]
@@ -13,16 +14,34 @@
     charts/histogram ;ヒストグラムを
     incanter/view) ;表示
 
-(def my-plot (charts/function-plot incanter/sin -10 10 )) ;-10から10の範囲にわたる正弦関数のプロット
+(def my-plot (charts/function-plot incanter/sin -10 10)) ;-10から10の範囲にわたる正弦関数のプロット
 (incanter/view my-plot) ;表示
-(incanter/save my-plot "plot.png") ;pngで保存
-(incanter.pdf/save-pdf my-plot "plot.pdf")  
-(incanter.svg/save-svg my-plot "plot.svg")  
+#_(incanter/save my-plot "plot.png") ;pngで保存
+#_(incanter.pdf/save-pdf my-plot "plot.pdf") 
+#_(incanter.svg/save-svg my-plot "plot.svg")
+
+(let [x (range 1 11)
+      y [1 2 4 3 4 5 5 4 3 5]]
+  (incanter/view
+   (charts/xy-plot x y)))
+
+(let [x (range 1 11)
+      y [1 2 4 3 4 5 5 4 3 5]]
+  (incanter/view
+   (charts/line-chart x y)))
+
+;2つのグラフ
+(let [x (range 1 11)
+      y1 [1 2 4 3 4 5 5 4 3 5]
+      y2 [2 3 4 5 6 5 4 3 2 1]
+      plot (charts/xy-plot x y1
+                           :series-label "sl1"
+                           :x-label "x"
+                           :y-label "y"
+                           :title "test-plot"
+                           :legend true)]
+  (charts/add-lines plot  x y2
+                    :series-label "sl2")  
+  (incanter/view plot))
 
 
-(comment
-  (charts/conj)
-  (incanter/conj-rows)
-  (status/sin 100)
-
-  )
